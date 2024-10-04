1b811abff57d529b95bfb4d1e164b5d1c0e9473a
Leo Ss <leo.vm.cba@gmail.com>
2024-10-04 10:45:37 -0300

    add: reactive form for new page hero

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Editorial } from '../../interfaces/hero.interface';
 
 @Component({
   selector: 'app-new-page',
@@ -6,10 +8,28 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class NewPageComponent {
+  public heroForm = new FormGroup({
+    id: new FormControl<string>(''),
+    superheroe: new FormControl<string>('', { nonNullable:true }), //Siempre va a ser un string
+    editorial: new FormControl<Editorial>( Editorial.MarvelComics ),
+    alter_ego: new FormControl<string>(''),
+    primera_aparicion: new FormControl<string>(''),
+    personajes: new FormControl<string>(''),
+    alt_image: new FormControl<string>(''),
+  });
+
   public publishers = [
     { id: 'Dc Comics', value: 'DC - Comics' },
     { id: 'Marvel Comics', value: 'Marvel - Comics' },
   ]
+
   constructor() { }
+
+  onSubmit(): void {
+    console.log( {
+      formIsValid: this.heroForm.valid,
+      value: this.heroForm.value
+    })
+  }
 
 }

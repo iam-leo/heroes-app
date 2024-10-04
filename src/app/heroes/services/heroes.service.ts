bae1e9084a469e788235bca96dd4150c8cfaa932
Leo Ss <leo.vm.cba@gmail.com>
2024-10-04 09:47:30 -0300

    add: CRUD heroes services

--- src/app/heroes/services/heroes.service.ts
+++ src/app/heroes/services/heroes.service.ts
@@ -24,14 +24,32 @@ export class HeroesService {
   }
 
   getSuggestions(query: string): Observable<Hero[]> {
-  return this.http.get<Hero[]>(`${this.apiUrl}/heroes`).pipe(
-    map((heroes: Hero[]) => {
-      return heroes.filter(hero => 
-        hero.superheroe.toLowerCase().includes(query.toLowerCase()) // Filtrado local en el frontend
-      );
-    }),
-    catchError(() => of([])) // Manejar errores devolviendo un array vacío
-  );
-}
+    return this.http.get<Hero[]>(`${this.apiUrl}/heroes`).pipe(
+      map((heroes: Hero[]) => {
+        return heroes.filter(hero => 
+          hero.superheroe.toLowerCase().includes(query.toLowerCase()) // Filtrado local en el frontend
+        );
+      }),
+      catchError(() => of([])) // Manejar errores devolviendo un array vacío
+    );
+  }
 
+  addHero( hero: Hero):Observable<Hero>{
+    return this.http.post<Hero>(`${this.apiUrl}/heroes`, hero);
+  }
+
+  updateHero( hero: Hero):Observable<Hero>{
+    if(!hero) throw Error('Hero id is required');
+
+    return this.http.patch<Hero>(`${this.apiUrl}/heroes/${ hero.id }`, hero);
+  }
+
+  deleteHero( id: string ):Observable<boolean>{
+
+    return this.http.delete(`${this.apiUrl}/heroes/${ id }`)
+      .pipe(
+        catchError( error => of(false)), //Error si no existe
+        map( resp => true) //Registro eliminado correctamente
+      )
+  }
 }

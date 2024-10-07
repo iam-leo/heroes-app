12a1f9cfc71cdb46543ffe1846910ea4eb8407ad
Leo Ss <leo.vm.cba@gmail.com>
2024-10-07 19:47:58 -0300

    add: check authentication function

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +27,19 @@ export class AuthService {
       tap( user => this.user = user ),
       tap( user => localStorage.setItem( 'token', 'asdadASD.Hfghgfh.RTrtyRuyUi456' ) ),
     )
+  }
+
+  checkAuthentication():Observable<boolean>{
+    if( !localStorage.getItem('token') ) return of(false);
+
+    const token = localStorage.getItem('token')
+    return this.http.get<User>(`${this.baseUrl}/usuarios/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false))
+      )
+
   }
 
   logout() {
